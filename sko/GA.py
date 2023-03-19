5711a8701c528745cc9a110f1dec17aa4eefb901
Levi Yan <leviyanx@gmail.com>
2023-03-19 11:46:15 +0800

    feat(GA): add function that run with less memory

--- sko/GA.py
+++ sko/GA.py
@@ -105,6 +105,39 @@ class GeneticAlgorithmBase(SkoBase, metaclass=ABCMeta):
         self.best_y = self.func(np.array([self.best_x]))
         return self.best_x, self.best_y
 
+    def run_with_less_memory(self, max_iter=None):
+        self.max_iter = max_iter or self.max_iter
+        best = []
+        for i in range(self.max_iter):
+            print("Iter {} begins...".format(i+1))
+            self.X = self.chrom2x(self.Chrom)
+            self.Y = self.x2y()
+            self.ranking()
+            self.selection()
+            self.crossover()
+            self.mutation()
+
+            # record the best ones
+            generation_best_index = self.FitV.argmax()
+            self.generation_best_X.append(self.X[generation_best_index, :])
+            self.generation_best_Y.append(self.Y[generation_best_index])
+            self.all_history_Y.append(self.Y)
+            # self.all_history_FitV.append(self.FitV)
+
+            if self.early_stop:
+                best.append(min(self.generation_best_Y))
+                if len(best) >= self.early_stop:
+                    if best.count(min(best)) == len(best):
+                        break
+                    else:
+                        best.pop(0)
+
+        global_best_index = np.array(self.generation_best_Y).argmin()
+        self.best_x = self.generation_best_X[global_best_index]
+        self.best_y = self.func(np.array([self.best_x]))
+
+        return self.best_x, self.best_y
+
     fit = run
 
 
